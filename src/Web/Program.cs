8b26c1599307d2e8d7a795de7b2ec21b474dbd76
Josiah <JosiahMatheson1@gmail.com>
2020-07-08 00:02:34 -0400

    Vscode config

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Web
+namespace FakeBook.Web
 {
     public class Program
     {
