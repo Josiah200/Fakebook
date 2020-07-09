6e2c5a870626123da51c9e2c3aa32346642766cb
Josiah <JosiahMatheson1@gmail.com>
2020-07-09 06:38:58 -0400

    Simple project structure

--- src/Web/Startup.cs
+++ src/Web/Startup.cs
@@ -16,6 +16,7 @@ namespace FakeBook.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,16 +25,15 @@ namespace FakeBook.Web
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+				app.UseStatusCodePages();
+				app.UseStaticFiles();
             }
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
