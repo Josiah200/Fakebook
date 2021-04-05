67086554c7a2e5e8f8a39df4bc8966fe5cbbb733
Josiah <JosiahMatheson1@gmail.com>
2021-04-05 02:49:54 -0400

    add ProfileData to User

--- src/Core/Entities/User.cs
+++ src/Core/Entities/User.cs
@@ -8,9 +8,22 @@ namespace Fakebook.Core.Entities
 		public string FirstName { get; set; }
 		public string LastName { get; set; }
 		public string PublicId { get; set; }
+		public ProfileData ProfileData { get; set; }
 		public List<Post> Posts { get; set; }
 		public ICollection<Friendship> Friendships { get; set; }
 		public List<Notification> Notifications { get; set; }
+
+	}
+
+	public class ProfileData : BaseEntity
+	{
+		public bool HasAvatar { get; set; }
+		public string Bio { get; set; }
+		public string City { get; set;}
+		public string Hometown { get; set; }
+		public string Workplace { get; set; }
+		public string HighSchool { get; set; }
+		public string College { get; set; }
 
 	}
 }
